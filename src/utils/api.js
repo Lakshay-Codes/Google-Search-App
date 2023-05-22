7c3d55aedb76805281b292deabbb1520809b9024
Lakshay <nijhawanlakshay06@gmail.com>
2023-05-22 14:21:04 +0530

    Initial commit

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+// https://developers.google.com/custom-search/v1/introduction
+
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
+
+const params = {
+  key: "AIzaSyDne5HRxb9RbBEM5L-5GfOtzY65DQporc4",
+  cx: "d2c5012151a7a4d61",
+};
+
+export const fetchDataFromApi = async (payload) => {
+  const { data } = await axios.get(BASE_URL, {
+    params: { ...params, ...payload },
+  });
+  return data;
+};
